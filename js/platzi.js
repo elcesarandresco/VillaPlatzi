89330328a78dccddb5005470ec8747bd3388df5e
elcesarandresco <cucesar6@gmail.com>
2021-01-03 00:18:58 -0500

    se terminó de poner las vacas en el canvas aleatorioamente

--- js/platzi.js
+++ js/platzi.js
@@ -1,13 +1,76 @@
-var z;
+var vp = document.getElementById("villaplatzi");
+var context = vp.getContext("2d");
 
 
-for(var i = 0; i <=10; i++) {
+var vaca = {
+    
+    url: "./img/vaca.png",
+    cargaOK: false,
+    
+};
 
-    z = aleatorio(5, 0);
-    document.write(z + ",");
-
+var fondo = {
+    
+    url: "./img/tile.png",
+    cargaOK: false
 
 }
+var cantidad = aleatorio(1, 50);
+
+fondo.img = new Image();
+fondo.img.src = fondo.url;
+fondo.img.addEventListener("load", cargarFondo);
+
+vaca.img = new Image();
+vaca.img.src = vaca.url
+vaca.img.addEventListener("load", cargarVacas);
+
+// pollo = new Image();
+// pollo.src = "./img/vaca.png"
+// pollo.addEventListener("load", cargarPollos);
+
+// cerdo = new Image();
+// cerdo.src = "./img/cerdo.png"
+// cerdo.addEventListener("load", cargarCerdos);
+
+function cargarFondo() {
+
+    fondo.cargaOK = true;
+    dibujar();
+
+}
+function cargarVacas() {
+
+    vaca.cargaOK = true;
+    dibujar();
+
+}
+
+
+function dibujar () {
+
+    if (fondo.cargaOK == true) {
+
+        context.drawImage(fondo.img, 0, 0);
+
+    }
+    if (vaca.cargaOK == true) {
+
+        for (var v=0; v <= cantidad; v++) {
+
+            var x = aleatorio(0, 6);
+            var y = aleatorio(0, 6);
+            var x = x * 80;
+            var y = y * 80;
+            context.drawImage(vaca.img, x, y);
+
+        }
+        console.log(cantidad);
+
+    }
+}
+
+
 
 
 function aleatorio (min, max) {
